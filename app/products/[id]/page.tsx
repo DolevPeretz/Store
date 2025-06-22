e70ae06ed007fa4718cf69b3dd64e15628204d38
DolevPeretz <dolevop@gmail.com>
2025-06-22 12:45:33 +0300

    dep fix

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,8 +5,9 @@ import { formatCurrency } from "@/utils/format";
 import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
 import AddToCart from "@/components/single-product/AddToCart";
 import ProductRating from "@/components/single-product/ProductRating";
-async function SingleProductPage({ params }: { params: { id: string } }) {
-  const product = await fetchSingleProduct({ ProductID: params.id });
+async function SingleProductPage(props: { params: Promise<{ id: string }> }) {
+  const { id } = await props.params;
+  const product = await fetchSingleProduct({ ProductID: id });
   const { name, image, company, description, price } = product;
   const dollarsAmount = formatCurrency(price);
   return (
@@ -28,15 +29,15 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
         <div>
           <div className="flex gap-x-8 items-center">
             <h1 className="capitalize text-3xl font-bold">{name}</h1>
-            <FavoriteToggleButton productID={params.id} />
+            <FavoriteToggleButton productID={id} />
           </div>
-          <ProductRating productId={params.id} />
+          <ProductRating productId={id} />
           <h4 className="text-xl mt-2">{company}</h4>
           <p className="mt-3 text-md bg-muted inline-block p-2 rounded-md">
             {dollarsAmount}
           </p>
           <p className="mt-6 leading-8 text-muted-foreground">{description}</p>
-          <AddToCart ProductID={params.id} />
+          <AddToCart ProductID={id} />
         </div>
       </div>
     </section>

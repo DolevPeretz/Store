3632cf91e4add81e37dcb752af274e923386ee6c
DolevPeretz <dolevop@gmail.com>
2025-06-25 15:51:40 +0300

    admin dashboards3

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "https://vgrcyxjkuudgauifbbeu.supabase.co",
+        hostname: "vgrcyxjkuudgauifbbeu.supabase.co",
       },
     ],
   },

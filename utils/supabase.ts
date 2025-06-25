2b51bd96a97411d621d6fdeeb8192729e6bc47ca
DolevPeretz <dolevop@gmail.com>
2025-06-25 10:26:29 +0300

    fix

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
 const bucket = "main-bucket";
-
+//fix
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_KEY as string
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY as string
 );
 
 export const uploadImage = async (image: File) => {

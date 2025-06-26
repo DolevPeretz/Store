974a8fa333b51b8776a3c80cc712e22afd4a0592
DolevPeretz <dolevop@gmail.com>
2025-06-26 13:54:50 +0300

    cart model

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,31 @@
-const CartPage = () => {
-  return <div>cart page</div>;
-};
+import CartItemsList from "@/components/cart/CartItemsList";
+import CartTotals from "@/components/cart/CartTotals";
+import SectionTitle from "@/components/global/SectionTitle";
+import { fetchOrCreateCart, updateCart } from "@/utils/actions";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+async function CartPage() {
+  const { userId } = await auth();
+  if (!userId) redirect("/");
+  const cart = await fetchOrCreateCart({ userId: userId });
+  const { currentCart, cartItems } = await updateCart(cart);
+  if (cartItems.length === 0) {
+    <SectionTitle text="Empty cart" />;
+  }
+  return (
+    <>
+      <SectionTitle text="Shopping Cart" />
+      <div className="mt-8 grid gap-4 lg:grid-cols-12">
+        <div className="lg:col-span-8">
+          <CartItemsList cartItems={cartItems} />
+        </div>
+        <div className="lg:col-span-4 lg:pl-4">
+          <CartTotals cart={currentCart} />
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default CartPage;
